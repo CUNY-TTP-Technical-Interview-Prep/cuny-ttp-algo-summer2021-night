4af8a195f5cc3acc77d41e5397900529c596ce74
[Chao Lai] <[chaolai.1082@gmail.com]>
2021-06-01 16:54:39 -0400

    lecture init

---
+++ week1/lecture/reshape-the-matrix/reshape-the-matrix.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=566 lang=python3
+#
+# [566] Reshape the Matrix
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+  def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+    
+# @lc code=end
+
