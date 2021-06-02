dc2bb8f624271e3c61e33b6008f580d96fae46bf
Ricky Chon <rickychon99@gmail.com>
2021-06-02 16:12:50 -0400

    Add space and time complexity info to solution

--- week1/assignments/to-lower-case/to_lowercase.py
+++ week1/assignments/to-lower-case/to_lowercase.py
@@ -1,3 +1,6 @@
+# Using builtins, easiest solution, but time and space complexity slower than most submissions
+# Runtime: 32 ms, faster than 39.87% of python3 submissions
+# Memory Usage: 14.3 MB, less than 32.25% of python3 submissions
 class Solution:
     def toLowerCase(self, s: str) -> str:
         return s.lower()
