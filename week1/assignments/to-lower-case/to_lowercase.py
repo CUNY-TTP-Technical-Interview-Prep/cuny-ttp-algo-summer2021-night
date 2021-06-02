c568334b51e865296fe89767e2ccf2fa0ece5275
Ricky Chon <rickychon99@gmail.com>
2021-06-02 16:49:19 -0400

    Condense custom toLowerCase implementation

--- week1/assignments/to-lower-case/to_lowercase.py
+++ week1/assignments/to-lower-case/to_lowercase.py
@@ -6,9 +6,9 @@ class Solution:
         return s.lower()
 
 # Custom implementation modifying ASCII values
-# Runtime: 32 ms, faster than 39.87% of python3 submissions
-# Memory Usage: 13.9 MB, less than 99.73% of python3 submissions
-class Solution2:
+# Runtime: 48 ms, faster than 39.87% of python3 submissions
+# Memory Usage: 14.3 MB, less than 32.25% of python3 submissions
+class Solution:
     def toLowerCase(self, s: str) -> str:
         # Converting uppercase letter to lowercase letter in ASCII means adding 32
         # A -> 65 and a -> 97, so 65 + 32 = 97
@@ -20,8 +20,5 @@ class Solution2:
 
         # Uppercase letter in ASCII ranges from 65 to 90 inclusive
         for ch in s:
-            if ord(ch) >= 65 and ord(ch) <= 90:
-                new_s += chr(ord(ch) + 32)
-            else:
-                new_s += ch
+            new_s += chr(ord(ch) + 32) if ord(ch) >= 65 and ord(ch) <= 90 else ch
         return new_s
