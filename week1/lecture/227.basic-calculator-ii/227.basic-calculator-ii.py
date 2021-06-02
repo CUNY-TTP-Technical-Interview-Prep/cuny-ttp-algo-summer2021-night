2f990b2505820eaa8ce59d112c95743d111a90e7
[Chao Lai] <[chaolai.1082@gmail.com]>
2021-06-02 16:59:16 -0400

    basic calc ii

---
+++ week1/lecture/227.basic-calculator-ii/227.basic-calculator-ii.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=227 lang=python3
+#
+# [227] Basic Calculator II
+#
+
+# @lc code=start
+class Solution:
+    def calculate(self, s: str) -> int:
+
+# @lc code=end
+
+# s = "3+2*2"
+# s = '3/2'
+# s = "14-3/2"
